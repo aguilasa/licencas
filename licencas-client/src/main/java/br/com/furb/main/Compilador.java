bd9c437764bcf1b45369010c37dc9e6aa17b2742
aguilasa <ingmar.aguiar@gmail.com>
2017-11-21 19:29:29 -0200

    Comp

--- licencas-client/src/main/java/br/com/furb/main/Compilador.java
+++ licencas-client/src/main/java/br/com/furb/main/Compilador.java
@@ -63,7 +63,7 @@ public class Compilador extends JFrame implements Observer, ActionListener {
 	 */
 	public Compilador() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 400, 246);
 		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
